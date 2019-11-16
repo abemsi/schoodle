71d8592ac94c64cfde6d3043fab148d6999fec77
Amy Bemister <amy.bemister@gmail.com>
2019-11-16 19:08:00 +0000

    Create seed data for all tables

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,127 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+-- Users table seeds
+INSERT INTO users (name, email)
+VALUES ('Alice', 'alicemurphy@gmail.com');
+
+INSERT INTO users (name, email)
+VALUES ('Kira', 'kiradon@gmail.com');
+
+INSERT INTO users (name, email)
+VALUES ('Bradley', 'bradbrad@gmail.com');
+
+INSERT INTO users (name, email)
+VALUES ('Rod', 'iliketoparty@gmail.com');
+
+INSERT INTO users (name, email)
+VALUES ('Denise', 'youlookshitty@gmail.com');
+
+-- Polls table seeds
+INSERT INTO polls (title, description, location, organizer_id)
+VALUE ('Birthday BONANZA', 'description', "Mom's Basement", 1);
+
+INSERT INTO polls (title, description, location, organizer_id)
+VALUE ('Offical Work Meeting', 'description', "Mcdonalds", 2);
+
+INSERT INTO polls (title, description, location, organizer_id)
+VALUE ('Badminton Practice', 'description', "The gym, duh", 3);
+
+INSERT INTO polls (title, description, location, organizer_id)
+VALUE ('Schoodle Opening Day', 'description', 'World Wide', 4);
+
+INSERT INTO polls (title, description, location, organizer_id)
+VALUE ('Group Midterm Project', 'description', "Lighthouse Labs", 5);
+
+-- Options table seeds
+INSERT INTO options (date, poll_id)
+VALUES ('2019-11-16', 1);
+INSERT INTO options (date, poll_id)
+VALUES ('2019-11-17', 1);
+INSERT INTO options (date, poll_id)
+VALUES ('2019-11-18', 1);
+
+INSERT INTO options (date, poll_id)
+VALUES ('2019-11-11', 2);
+INSERT INTO options (date, poll_id)
+VALUES ('2019-11-12', 2);
+INSERT INTO options (date, poll_id)
+VALUES ('2019-11-13', 2);
+
+INSERT INTO options (date, poll_id)
+VALUES ('2019-11-13', 3);
+INSERT INTO options (date, poll_id)
+VALUES ('2019-11-14', 3);
+INSERT INTO options (date, poll_id)
+VALUES ('2019-11-15', 3);
+
+INSERT INTO options (date, poll_id)
+VALUES ('2019-11-20', 4);
+INSERT INTO options (date, poll_id)
+VALUES ('2019-11-21', 4);
+INSERT INTO options (date, poll_id)
+VALUES ('2019-11-22', 4);
+
+INSERT INTO options (date, poll_id)
+VALUES ('2019-11-23', 5);
+INSERT INTO options (date, poll_id)
+VALUES ('2019-11-24', 5);
+INSERT INTO options (date, poll_id)
+VALUES ('2019-11-25', 5);
+
+-- Choices table seeds
+INSERT INTO choices (option_id, attendee_id, poll_id)
+VALUES (1, 2, 1);
+INSERT INTO choices (option_id, attendee_id, poll_id)
+VALUES (2, 2, 1);
+INSERT INTO choices (option_id, attendee_id, poll_id)
+VALUES (1, 1, 1);
+INSERT INTO choices (option_id, attendee_id, poll_id)
+VALUES (2, 1, 1);
+
+INSERT INTO choices (option_id, attendee_id, poll_id)
+VALUES (1, 2, 2);
+INSERT INTO choices (option_id, attendee_id, poll_id)
+VALUES (1, 1, 2);
+INSERT INTO choices (option_id, attendee_id, poll_id)
+VALUES (2, 3, 2);
+
+INSERT INTO choices (option_id, attendee_id, poll_id)
+VALUES (1, 2, 3);
+INSERT INTO choices (option_id, attendee_id, poll_id)
+VALUES (2, 1, 3);
+INSERT INTO choices (option_id, attendee_id, poll_id)
+VALUES (2, 3, 3);
+
+INSERT INTO choices (option_id, attendee_id, poll_id)
+VALUES (1, 2, 4);
+INSERT INTO choices (option_id, attendee_id, poll_id)
+VALUES (1, 1, 4);
+INSERT INTO choices (option_id, attendee_id, poll_id)
+VALUES (2, 3, 4);
+
+INSERT INTO choices (option_id, attendee_id, poll_id)
+VALUES (1, 2, 5);
+INSERT INTO choices (option_id, attendee_id, poll_id)
+VALUES (1, 1, 5);
+INSERT INTO choices (option_id, attendee_id, poll_id)
+VALUES (1, 3, 5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
