0a71e6e2d4482c12e7a52b92a63c98faf5f8b2ce
Amy Bemister <amy.bemister@gmail.com>
2019-11-16 18:14:32 +0000

    Create tables for database

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,37 @@
--- Drop and recreate Users table (Example)
+-- Drop and recreate tables
 
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS  polls CASCADE;
+DROP TABLE IF EXISTS options CASCADE;
+DROP TABLE IF EXISTS choices CASCADE;
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE polls (
+  id SERIAL PRIMARY KEY NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  location VARCHAR(255),
+  organizer_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE options {
+  id SERIAL PRIMARY KEY NOT NULL,
+  date DATE NOT NULL,
+  poll_id INTEGER REFERENCES polls(id) ON DELETE CASCADE
+};
+
+CREATE TABLE choices {
+  id SERIAL PRIMARY KEY NOT NULL,
+  option_id INTEGER REFERENCES options(id) ON DELETE CASCADE,
+  attendee_id INTEGER REFERENCES users(id) ON DELETE CASCADE;
+  poll_id INTEGER REFERENCES polls(id) ON DELETE CASCADE;
+};
+
+
+
+
