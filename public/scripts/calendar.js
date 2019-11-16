de2a62db5d0e2b2b21a4282ca2794bcf37f69866
Amy Bemister <amy.bemister@gmail.com>
2019-11-16 22:37:37 +0000

    Add jquery to make calendar clickable and alert the date clicked

--- public/scripts/calendar.js
+++ public/scripts/calendar.js
@@ -1,7 +1,27 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  $('#my-calendar').zabuto_calendar({
+      language: 'en',
+      data: [
+          {
+              'date': '2018-01-11',
+              'badge': true,
+              'title': 'Meeting with John'
+          },
+          {
+              'date': '2018-01-13',
+              'badge': false,
+              'title': 'Go to market'
+          }
+      ],
+      action: function () {
+          //get the selected date
+          var date = $('#' + this.id).data('date');
+          //alert the date
+          alert('You clicked date: ' + date);
+        },
 
+      eventClick: function(date) {
+        $(this).css('border-color', 'orange');
+      }
+  });
 });
-
-
-
-
