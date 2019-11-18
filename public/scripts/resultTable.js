c46b924e77ce5660311c90178029979f43f1d044
nolan <nolan796@live.ca>
2019-11-18 15:41:21 -0700

    Merge branch 'views' of github.com:abemsi/schoodle into views

---
+++ public/scripts/resultTable.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  $(".add-row").click(function(){
+    let name = $("#name").val();
+    let email = $("#email").val();
+    let markup = "<tr><td>" + name + " " + email + "</td>" + "<td></td>" + "<td></td></tr>";
+    $("table tbody").append(markup);
+  });
+
+});
+
