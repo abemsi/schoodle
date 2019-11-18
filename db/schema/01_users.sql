79dff97cf5fe3068712b9db28a112a49b01ed51a
Amy Bemister <amy.bemister@gmail.com>
2019-11-18 17:29:07 +0000

    Add link column to polls table

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -16,6 +16,7 @@ CREATE TABLE polls (
   title VARCHAR(255) NOT NULL,
   description TEXT,
   location VARCHAR(255),
+  link VARCHAR(255),
   organizer_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
 
