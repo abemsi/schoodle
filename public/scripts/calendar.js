a557f28db34a95a2f4c9a1f98c5f814fa77d4c08
nolan <nolan796@live.ca>
2019-11-19 14:19:07 -0700

    Logged clicked date instead of alert

--- public/scripts/calendar.js
+++ public/scripts/calendar.js
@@ -1,28 +1,30 @@
 $(document).ready(function () {
-  $('#my-calendar').zabuto_calendar({
+
+
+    $('#my-calendar').zabuto_calendar({
       language: 'en',
       data: [
           {
-              'date': '2018-01-11',
+              'date': '2019-11-11',
               'badge': true,
               'title': 'Meeting with John'
           },
           {
-              'date': '2018-01-13',
+              'date': '2019-11-13',
               'badge': false,
               'title': 'Go to market'
           }
       ],
       action: function () {
           //get the selected date
-          let date = $('#' + this.id).data('date');
+          var date = $('#' + this.id).data('date');
           //alert the date
           $('#calendar-dates').val(date);
-          alert('You clicked date: ' + date);
+          console.log('You clicked date: ' + date);
         },
 
       eventClick: function(date) {
-        $(this).css('background-color', 'orange');
+       
       }
   });
 });
