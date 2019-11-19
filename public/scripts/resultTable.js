cb233c60f68e3526b04f64ded1022dac1968e370
Amy Bemister <amy.bemister@gmail.com>
2019-11-19 04:04:56 +0000

    Experiment with table layout

--- public/scripts/resultTable.js
+++ public/scripts/resultTable.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   $(".add-row").click(function(){
     let name = $("#name").val();
     let email = $("#email").val();
-    let markup = "<tr><td>" + name + " " + email + "</td>" + "<td></td>" + "<td></td></tr>";
+    let markup = "<tr><td>" + name + " " + email + "</td>" + "<td></td>" + "<td></td>" + "<td></td>";
     $("table tbody").append(markup);
   });
 
