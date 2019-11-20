74beab5c87793ac7fccda8323f6b97dd82045425
nolan295 <nolan796@live.ca>
2019-11-20 21:25:06 +0000

    Edit random string generator inside helper file

--- helpers.js
+++ helpers.js
@@ -1,7 +1,9 @@
 // Returns a string of 16 random alphanumeric characters for a unique shortURL
 const generateRandomString = function() {
-  let randomString = Math.random().toString(36).substring(16); 
-  return randomString
-};
+let random = Math.random().toString(36).substring(2);
+// console.log("random", random += random);
+return random += random;
+}
 
+generateRandomString();
 module.exports = { generateRandomString };
