0997eb8edf150fb8ec4aa54bc6773a78b6a4f05c
Amy Bemister <amy.bemister@gmail.com>
2019-11-20 02:39:53 +0000

    add/remove class to highlight date

--- public/scripts/calendar.js
+++ public/scripts/calendar.js
@@ -1,21 +1,21 @@
 $(document).ready(function () {
 
     let dates = [
-      {
-          'date': '2019-11-11',
-          'badge': true,
-          'title': 'Meeting with John'
-      },
-      {
-          'date': '2019-11-13',
-          'badge': false,
-          'title': 'Go to market'
-      },
-      {
-        'date': '2019-11-15',
-        'badge': false,
-        'title': 'Go to market'
-      }
+      // {
+      // //     'date': '2019-11-11',
+      // //     'badge': true,
+      // //     'title': 'Meeting with John'
+      // // },
+      // // {
+      // //     'date': '2019-11-13',
+      // //     'badge': false,
+      // //     'title': 'Go to market'
+      // // },
+      // // {
+      // //   'date': '2019-11-15',
+      // //   'badge': false,
+      // //   'title': 'Go to market'
+      // }
   ];
     $('#my-calendar').zabuto_calendar({
       language: 'en',
@@ -24,14 +24,22 @@ $(document).ready(function () {
           //get the selected date
           var date = $('#' + this.id).data('date');
           //alert the date
-          myFunction(dates, date);
+          myFunction(dates, date, this);
         }
-        
+
   });
-  let myFunction = function (data, date) {
+  let myFunction = function (data, date, dayElement) {
     console.log(data);
-    data.push({'date': date, 'badge': false, 'title': 'test'});
+    if (dayElement.classList.contains('event')) {
+      $(dayElement).removeClass('event');
+    } else {
+      data.push({'date': date, 'badge': false, 'title': 'test'});
+      $(dayElement).addClass('event');
+    }
   };
+
+
+
 });
 
 
