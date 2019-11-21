8bb7e5dda8e4f515cc3e6b2c118aa3a300a91ec4
nolan295 <nolan796@live.ca>
2019-11-21 19:05:08 +0000

    Fix indentations

--- routes/users.js
+++ routes/users.js
@@ -12,25 +12,27 @@ module.exports = (db) => {
 
   router.post("/", (req, res) => {
     const pollData = req.body;
-    db.addUser(pollData, function (rows) {     
-     const newUser = rows[0];
-      if (!newUser) {
-        res.send({error: "error"});
-        return;
-      };
-      req.session.user_id = newUser.id;
-      let pollpayload = {...pollData, organizer_id: newUser.id };
-      
-      db.addPoll(pollpayload).then(function (rows) {     
-        const newPoll = rows[0];
-        if (!newPoll) {
+      db.addUser(pollData, function (rows) {     
+        const newUser = rows[0];
+          if (!newUser) {
           res.send({error: "error"});
           return;
-        };
+         };
+      
+      // Add new poll - running function to insert: title, description, location of poll
+      req.session.user_id = newUser.id;
+      let pollpayload = {...pollData, organizer_id: newUser.id };
+      db.addPoll(pollpayload).then(function (rows) {     
+        const newPoll = rows[0];
+          if (!newPoll) {
+            res.send({error: "error"});
+            return;
+          };
 
+        // Split our dates from req.body, run function to add them to database
         const calendarDates = pollData['calendar-dates'].split(',')
         calendarDates.map(function(date) {
-          let optionspayload = {calendarDate: date, poll_id: newPoll.id }
+        let optionspayload = {calendarDate: date, poll_id: newPoll.id }
           db.addOption(optionspayload, function (err, results) {     
             if (err) {
               console.error(err.message);
