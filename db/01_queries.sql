4b212d622fffd5150d149fcaa806d7361713766a
nolan295 <nolan796@live.ca>
2019-11-21 03:55:19 +0000

    comment out queries

--- db/01_queries.sql
+++ db/01_queries.sql
@@ -6,12 +6,15 @@
 -- WHERE link = '6nnqe248p6k6nnqe248p6k'
 -- GROUP BY polls.id, options.id, users.id;
 
-SELECT users.name AS name, users.email AS email, options.date AS choice, users.id AS attendeeID
-FROM choices
-JOIN polls ON polls.id = poll_id
-JOIN users ON users.id = choices.attendee_id
-JOIN options ON options.id = choices.option_id
-WHERE link = '6nnqe248p6k6nnqe248p6k';
+-- SELECT users.name AS name, users.email AS email, options.date AS choice, users.id AS attendeeID
+-- FROM choices
+-- JOIN polls ON polls.id = poll_id
+-- JOIN users ON users.id = choices.attendee_id
+-- JOIN options ON options.id = choices.option_id
+-- WHERE link = '6nnqe248p6k6nnqe248p6k';
+
+
+
 
 
 /*
