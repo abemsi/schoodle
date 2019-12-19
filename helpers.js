688c2063522a04be21ca845dc92d94d912ae0319
nolan <nolan796@live.ca>
2019-12-19 12:43:38 -0700

    Remove c.log

--- helpers.js
+++ helpers.js
@@ -1,7 +1,7 @@
 // Returns a string of 16 random alphanumeric characters for a unique shortURL
 const generateRandomString = function() {
 let random = Math.random().toString(36).substring(2);
-// console.log("random", random += random);
+
 return random += random;
 }
 
